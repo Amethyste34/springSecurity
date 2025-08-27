cfe6db7659cd3e4c5a566cc1cef75878e383d06a
Amethyste34 <laurence.amethyste@gmail.com>
2025-08-27 12:26:26 +0200

    TP1a

--- src/main/java/fr/diginamic/demo/HelloController.java
+++ src/main/java/fr/diginamic/demo/HelloController.java
@@ -1,17 +1,25 @@
 package fr.diginamic.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
-    @GetMapping("/hello-public")
+    @GetMapping("/public")
     public String HelloPublic() {
         return "Hello public";
     }
 
-    @GetMapping("/hello-private")
+    @GetMapping("/private")
     public String helloPrivate() {
         return "Hello private";
+    }
+
+    @PostMapping("/public")
+    public String helloPublicPost() {
+        return "Hello public (POST)";
     }
 }
--- src/main/java/fr/diginamic/demo/SecurityConfig.java
+++ src/main/java/fr/diginamic/demo/SecurityConfig.java
@@ -1,4 +1,20 @@
 package fr.diginamic.demo;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/hello/public").permitAll()
+                        .requestMatchers("/hello/private").authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
 }
